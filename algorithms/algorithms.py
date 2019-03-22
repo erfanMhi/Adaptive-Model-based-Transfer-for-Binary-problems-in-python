a923e56eada3491ed5fc2b4f45643a2ac077d7d9
erfan <mhi.erfan1@gmail.com>
2019-03-22 18:44:13 +0430

    New evolutionary algorithms for finding the optimal solution to the knapsack problem added.

--- algorithms/algorithms.py
+++ algorithms/algorithms.py
@@ -1,12 +1,12 @@
-
+import os
 import numpy as np
 
 from utils.tools import Tools
-from utils.fitness_util import fitness_eval
+from utils.fitness_util import *
 from probability_models.probability_model import ProbabilityModel
 from probability_models.mixture_model import MixtureModel
 
-def BGA(problem, dims, th_best):
+def BGA(problem, dims, th_best, pop=200, gen=1000, addr="problems/toy_problem"):
     """[bestSol, fitness_hist] = BGA(problem,dims,th_best): simple binary GA with
         uniform crossover and bit-flip mutation. 
        INPUT:
@@ -19,14 +19,12 @@ def BGA(problem, dims, th_best):
          bestSol: best solution
          fitness: history of best fitness for each generation"""
     bestSol = None
-    all_models = Tools.load_from_file('all_models')
+    all_models = Tools.load_from_file(os.path.join(addr, 'all_models'))
     
-    pop = 200
-    gen = 1000
     
     fitness_hist = np.zeros(gen)
     population = np.round(np.random.rand(pop,dims))    
-    fitness = fitness_eval(population,problem,dims);
+    fitness = fitness_eval(population, problem, dims);
     buildmodel = True;
     fitness_hist[0] = max(fitness);
     print('Generation 0 best fitness = ',str(fitness_hist[0]));
@@ -59,35 +57,35 @@ def BGA(problem, dims, th_best):
             model = ProbabilityModel('umd')
             model.buildmodel(population)
             all_models.append(model)
-            Tools.save_to_file('all_models',all_models)
+            Tools.save_to_file(os.path.join(addr, 'all_models'), all_models)
             print('Complete!')
             fitness_hist[i+1:] = fitness[0];
             bestSol = population[0, :];
             break
     return bestSol, fitness_hist
 
-def AMT_BGA(problem, dims, reps, trans):
+def AMT_BGA(problem, dims, reps, trans, fitness_func='toy_problem', addr="problems/toy_problem"):
     """[bestSol, fitness_hist, alpha] = TSBGA(problem, dims, reps, trans): Adaptive
-    %Model-based Transfer Binary GA. The crossover and mutation for this simple
-    %binary GA are uniform crossover and bit-flip mutation.
-    %INPUT:
-    % problem: problem type, 'onemax', 'onemin', or 'trap5'
-    % dims: problem dimensionality
-    % reps: number of repeated trial runs
-    % trans:    trans.transfer: binary variable 
-    %           trans.TrInt: transfer interval for AMT
-    %
-    %OUTPUT:
-    % bestSol: best solution for each repetiion
-    % fitness: history of best fitness for each generation
-    % alpha: transfer coefficient
+        Model-based Transfer Binary GA. The crossover and mutation for this simple
+        binary GA are uniform crossover and bit-flip mutation.
+        INPUT:
+         problem: problem type, 'onemax', 'onemin', or 'trap5'
+         dims: problem dimensionality
+         reps: number of repeated trial runs
+         trans:    trans.transfer: binary variable 
+                   trans.TrInt: transfer interval for AMT
+        
+        OUTPUT:
+         bestSol: best solution for each repetiion
+         fitness: history of best fitness for each generation
+         alpha: transfer coefficient
     """
     pop = 50
     gen = 100
     transfer = trans['transfer']
     if transfer:
         TrInt = trans['TrInt']
-        all_models = Tools.load_from_file('all_models')
+        all_models = Tools.load_from_file(os.path.join(addr, 'all_models'))
         
     fitness_hist = np.zeros((reps, gen))
     bestSol = np.zeros((reps, dims))
@@ -96,7 +94,10 @@ def AMT_BGA(problem, dims, reps, trans):
     for rep in range(reps):
         alpha_rep = []
         population = np.round(np.random.rand(pop, dims))
-        fitness = fitness_eval(population,problem,dims)
+        if fitness_func == 'toy_problem':
+            fitness = fitness_eval(population,problem,dims)
+        else:
+            fitness = knapsack_fitness_eval(population,problem,dims,pop)
         ind = np.argmax(fitness)
         best_fit = fitness[ind]
         print('Generation 0 best fitness = ',str(best_fit))
@@ -124,8 +125,11 @@ def AMT_BGA(problem, dims, reps, trans):
                 tmp = np.random.rand(pop,dims)
                 index = tmp<(1/dims)
                 offspring[index] = np.abs(1-offspring[index])
-                    
-            cfitness = fitness_eval(offspring,problem,dims)
+             
+            if fitness_func == 'toy_problem':
+                cfitness = fitness_eval(population,problem,dims)
+            else:
+                cfitness = knapsack_fitness_eval(population,problem,dims,pop)
             interpop = np.append(population,offspring,0)
             interfitness = np.append(fitness,cfitness)
             index = np.argsort((-interfitness))
@@ -139,3 +143,65 @@ def AMT_BGA(problem, dims, reps, trans):
         alpha[rep] = alpha_rep
         bestSol[rep, :] = population[ind, :]
     return bestSol, fitness_hist, alpha
+
+def KP_BGA(problem, dims, premSTOP, pop=100, gen=100, addr="problems/toy_problem"):
+    """[bestSol, fitness_hist] = BGA(problem,dims,th_best): simple binary GA with
+        uniform crossover and bit-flip mutation. 
+        INPUT:
+         problem: problem generated by GenKnapsack
+         dims: problem dimensionality
+         premSTOP: used for early stop if no improvement is made for 20
+         consecutive generations
+        
+        OUTPUT:
+         bestSol: best solution
+         fitness: history of best fitness for each ge  neration"""
+    bestSol = None
+    all_models = Tools.load_from_file(os.path.join(addr, 'all_models'))
+    
+    
+    fitness_hist = np.zeros(gen)
+    population = np.round(np.random.rand(pop,dims))    
+    fitness = knapsack_fitness_eval(population, problem, dims,pop);
+    fitness_hist[0] = bestfitness = max(fitness);
+    print('Generation 0 best fitness = ',str(fitness_hist[0]));
+    
+    counter = 0
+    for i in range(1,gen):
+        parent1 = population[np.random.permutation(pop),:];
+        parent2 = population[np.random.permutation(pop),:];
+        tmp = np.random.rand(pop,dims)
+        offspring = np.zeros((pop,dims));
+        index = tmp>=0.5;
+        offspring[index] = parent1[index];
+        index = tmp<0.5;
+        offspring[index] = parent2[index];
+        tmp = np.random.rand(pop,dims);
+        index = tmp<(1/dims);
+        offspring[index] = np.abs(1-offspring[index]);
+        cfitness = knapsack_fitness_eval(population, problem, dims,pop);
+        interpop = np.append(population,offspring,0)
+        interfitness = np.append(fitness,cfitness)
+        index = np.argsort((-interfitness));
+        interfitness = interfitness[index]
+        fitness = interfitness[:pop]
+        interpop = interpop[index,:]        
+        population = interpop[:pop,:]
+        fitness_hist[i] = fitness[0];
+        print('Generation ', str(i), ' best fitness = ',str(fitness_hist[i])) 
+ 
+        if fitness[0] > bestfitness:
+            bestfitness = fitness[0];
+            counter = 0;
+        else:
+            counter += 1
+        if counter == 20 and premSTOP:
+            fitness_hist[i:] = fitness_hist[i];
+            break;
+    bestSol = population[0, :];
+    model = ProbabilityModel('umd')
+    model.buildmodel(population)
+    all_models.append(model)
+    Tools.save_to_file(os.path.join(addr, 'all_models'), all_models)
+       
+    return bestSol, fitness_hist
